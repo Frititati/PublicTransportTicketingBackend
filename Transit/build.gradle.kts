d70fcb6448261cd191a395663ea9a8e32debddbb
francescoscalera99 <francesoscalera99@gmail.com>
2022-10-18 18:42:52 +0200

    fixed build.gradle.kts

--- Transit/build.gradle.kts
+++ Transit/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-        plugins {
-            id("org.springframework.boot") version "2.6.7"
-            id("io.spring.dependency-management") version "1.0.11.RELEASE"
-            kotlin("jvm") version "1.6.21"
-            kotlin("plugin.spring") version "1.6.21"
-            kotlin("plugin.jpa") version "1.6.21"
-        }
+plugins {
+    id("org.springframework.boot") version "2.6.7"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+}
 
 group = "it.polito.wa2"
 version = "0.0.1-SNAPSHOT"
